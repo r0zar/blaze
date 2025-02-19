7156aaf8ddda903973a3670321ebc71292af5b9a
Ross Ragsdale <ross@pointblankdev.com>
2025-02-19 15:30:13 -0600

    :art: Update

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-    entry: ['src/index.ts'],
+    entry: [
+        'src/index.ts',
+        'src/client/index.ts',
+        'src/server/index.ts'
+    ],
     format: ['cjs', 'esm'],
     dts: true,
     splitting: false,
